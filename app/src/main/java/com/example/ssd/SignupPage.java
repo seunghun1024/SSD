21cea6e146aafb29ec6e2ac2ea45262b6881b3d8
seunghun1024 <qotmdgns123@naver.com>
2019-04-17 17:51:32 +0900

    add insert() DB 저장 가능 ID 중복체크 가능

--- app/src/main/java/com/example/ssd/SignupPage.java
+++ app/src/main/java/com/example/ssd/SignupPage.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -21,6 +22,12 @@ import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -52,7 +59,7 @@ public class SignupPage extends AppCompatActivity {
         btninsert.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-              // insert();
+               insert();
             }
         });
 
@@ -143,5 +150,105 @@ public class SignupPage extends AppCompatActivity {
         }
         OverlapCheck task2 = new OverlapCheck();
         task2.execute();
+    }
+
+    //-----------------------------회원가입 눌렀을 시 메서드---------------------------------------------------
+    public void insert() {
+        String Id = editTextId.getText().toString();
+        String Pw = editTextPw.getText().toString();
+        String Name = editTextName.getText().toString();
+        String Phone = editTextPhone.getText().toString();
+
+        if (!validate) {
+            AlertDialog.Builder alert4 = new AlertDialog.Builder(SignupPage.this);
+            alert4.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();     //닫기
+                }
+            });
+            alert4.setMessage("아이디 중복체크를 하지 않았습니다.");
+            alert4.show();
+            return;
+        }
+
+        if (Id.equals("") || Pw.equals("") || Name.equals("") || Phone.equals("")) {
+            AlertDialog.Builder alert5 = new AlertDialog.Builder(SignupPage.this);
+            alert5.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();     //닫기
+                }
+            });
+            alert5.setMessage("빈 칸을 다 채워주시기 바랍니다.");
+            alert5.show();
+            return;
+        }
+
+        insertoToDatabase(Id, Pw, Name, Phone);
+    }
+
+    private void insertoToDatabase(String Id, String Pw, String Name, String Phone) {
+        class InsertData extends AsyncTask<String, Void, String> {
+            // ProgressDialog loading;
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                //  loading = ProgressDialog.show(SignupPage2.this, "Please Wait", null, true, true);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                // loading.dismiss();
+                System.out.println("백그라운드 끝");
+                Intent mainpage = new Intent(SignupPage.this, MainActivity.class);
+                startActivity(mainpage);
+            }
+
+            @Override
+            protected String doInBackground(String... params) {
+                try {
+                    String Id = (String) params[0];
+                    String Pw = (String) params[1];
+                    String Name = (String) params[2];
+                    String Phone = (String) params[3];
+
+                    String link = "http://203.234.62.96:7979/signup.php";
+                    String data = URLEncoder.encode("Id", "UTF-8") + "=" + URLEncoder.encode(Id, "UTF-8");
+                    data += "&" + URLEncoder.encode("Pw", "UTF-8") + "=" + URLEncoder.encode(Pw, "UTF-8");
+                    data += "&" + URLEncoder.encode("Name", "UTF-8") + "=" + URLEncoder.encode(Name, "UTF-8");
+                    data += "&" + URLEncoder.encode("Phone", "UTF-8") + "=" + URLEncoder.encode(Phone, "UTF-8");
+
+                    System.out.println(Id + Pw + Name + Phone);
+
+                    URL url = new URL(link);
+                    URLConnection conn = url.openConnection();
+
+                    conn.setDoOutput(true);
+                    OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+
+                    wr.write(data);
+                    wr.flush();
+
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+                    StringBuilder sb = new StringBuilder();
+                    String line = null;
+
+                    // Read Server Response
+                    while ((line = reader.readLine()) != null) {
+                        sb.append(line);
+                        break;
+                    }
+                    return sb.toString();
+                } catch (Exception e) {
+                    return new String("Exception: " + e.getMessage());
+                }
+            }
+        }
+        InsertData task = new InsertData();
+        task.execute(Id, Pw, Name, Phone);
     }
 }
